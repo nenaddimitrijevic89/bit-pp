811dc435cc26693b168af939fcf8289b3d11a239
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-03 00:18:13 +0100

    0303200018

---
+++ practice/15.functions_6.js
@@ -0,0 +1,21 @@
+/****     1    ****/
+
+// var countVowels = function(str) {
+//     var count = 0;
+//     var a = str.toUpperCase();
+//     for (var i = 0; i < str.length; i++) {
+//         if (
+//             a[i] === "A" ||
+//             a[i] === "E" ||
+//             a[i] === "I" ||
+//             a[i] === "O" ||
+//             a[i] === "U"
+//         ) {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+// console.log(countVowels("nenad"));
+
+/****         ****/
