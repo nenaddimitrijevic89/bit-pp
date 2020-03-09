ae7ca52bee6f6f6c559cf4eebf2a4606f6afc155
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-09 23:25:32 +0100

    0903201125

---
+++ practice/2. Functions exercises/7. functions_7.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/****   1    ****/
+
+// var hasDigit=(function(str){
+//     for(var i=0; i<str.length; i++){
+//         if(str[i]==5){
+//             return true;
+//         }
+//     }
+//     return false;
+// })("1b895abd”")
+
+// console.log(hasDigit);
+
+/****   2   ****/
+
+// var replaceLetter = (function(str) {
+//   var newStr = str.replace(/JS/g, "**");
+//   return newStr;
+// })("Programming in JS is super JS interesting");
+
+// console.log(replaceLetter);
+
+/****   3   ****/
+
+// var insertCharacter = (function(str, el, p) {
+//   var newStr = "";
+//   for (var i = 0; i < str.length; i++) {
+//     if (i === p) {
+//       newStr += el;
+//     }
+//     newStr += str[i];
+//   }
+//   return newStr;
+// })("Goo morning", "d", 3);
+// console.log(insertCharacter);
+
+/****   4   ****/
+
+// var deleteCharacter = (function(str, p) {
+//   var newStr = "";
+//   for (var i = 0; i < str.length; i++) {
+//     if (i === p) {
+//       continue;
+//     } else {
+//       newStr += str[i];
+//     }
+//   }
+//   return newStr;
+// })("Goodd morning", 3);
+
+// console.log(deleteCharacter);
+
--- practice/4. OOP exercises/7. bookingHouse.js
+++ practice/4. OOP exercises/7. bookingHouse.js
@@ -1,65 +1,179 @@
-"use strict"
-
-// (function () {
-function Country(name, odds, continent) {
+"use strict";
+(function () {
+  function Country(name, odds, continent) {
     this.name = name;
     this.odds = odds;
     this.continent = continent;
-}
-
-function Person(name, surname, dateOfBird) {
+  }
+  function Person(name, surname, dateOfBird) {
     this.name = name;
     this.surname = surname;
     this.dateOfBird = new Date(dateOfBird);
-}
-function Player(person, betAmount, country) {
+  }
+  function Player(person, betAmount, country) {
     this.person = person;
     this.betAmount = betAmount;
     this.country = country;
-}
-function Address(country, city, postalCode, street, number) {
+  }
+  function Address(country, city, postalCode, street, number) {
     this.country = country;
     this.city = city;
     this.postalCode = postalCode;
     this.street = street;
     this.number = number;
-}
-function BettingPlace(address) {
+  }
+  function BettingPlace(address) {
     this.address = address;
-    this.listOfPlayer = [];
+    this.listOfPlayers = [];
     this.numberOfPlayers = 0;
-}
-function BettingHouse(competition) {
+  }
+  function BettingHouse(competition) {
     this.competition = competition;
     this.listOfBettingPlace = [];
     this.numberOfPlayers = 0;
+  }
 
-}
-var CONTINENT = Object.freeze({
+  var CONTINENT = Object.freeze({
     EUROPE: "EU",
     ASIA: "AS",
     AFRICA: "AF",
     SOUTHAMERICA: "SA",
     NORTHAMERICA: "NA",
-    AUSTRALIA: "AU",
+    AUSTRALIA: "AU"
+  });
 
-});
-Person.prototype.getData = function () {
+  Person.prototype.getData = function () {
     var day = this.dateOfBird.getDate();
     var month = this.dateOfBird.getMonth() + 1;
     var year = this.dateOfBird.getFullYear();
-    return this.name + " " + this.surname + ", " + day + "." + month + "." + year + ".";
-}
+    return (
+      this.name +
+      " " +
+      this.surname +
+      ", " +
+      day +
+      "." +
+      month +
+      "." +
+      year +
+      "."
+    );
+  };
 
-Address.prototype.getData = function () {
-    return this.street + this.number + ", " + this.postalCode + this.city + ", "
-}
+  Person.prototype.getYearsOld = function () {
+    var year = this.dateOfBird.getFullYear();
+    var today = new Date().getFullYear();
+    return today - year;
+  };
 
+  Address.prototype.getData = function () {
+    return (
+      this.street +
+      " " +
+      this.number +
+      ", " +
+      this.postalCode +
+      " " +
+      this.city +
+      ", " +
+      this.country.name.slice(0, 3).toUpperCase()
+    );
+  };
 
-var personOne = new Person("nenad", "dimitrijevic", "1/23/89")
-console.log(personOne.getData());
+  Player.prototype.getData = function () {
+    return (
+      this.country.name.slice(0, 3).toUpperCase() +
+      ", " +
+      this.betAmount.toFixed(2) +
+      ", " +
+      this.person.name +
+      " " +
+      this.person.surname +
+      ", " +
+      this.person.getYearsOld() +
+      " years"
+    );
+  };
 
+  BettingPlace.prototype.addPlayers = function (player) {
+    this.listOfPlayers.push(player);
+    this.numberOfPlayers++;
+  };
 
+  BettingHouse.prototype.addbettingPlaces = function (place) {
+    this.listOfBettingPlace.push(place);
+    this.numberOfPlayers += place.numberOfPlayers;
+  };
 
+  BettingHouse.prototype.getData = function () {
+    return (
+      this.competition +
+      ", " +
+      this.listOfBettingPlace.length +
+      " betting places, " +
+      this.numberOfPlayers +
+      " bets\n"
+    );
+  };
 
-    // })()
+  var createPlayer = function (
+    name,
+    surname,
+    dateOfBird,
+    countryName,
+    odds,
+    continent,
+    bet
+  ) {
+    var per = new Person(name, surname, dateOfBird);
+    var coun = new Country(countryName, odds, continent);
+    return new Player(per, bet, coun);
+  };
+
+  var createBettingPlace = function (country, city, postalCode, street, number) {
+    var add = new Address(country, city, postalCode, street, number);
+    return new BettingPlace(add);
+  };
+
+  var countryOne = new Country("Serbia", 1.5, CONTINENT.EUROPE);
+  var personOne = new Person("Nenad", "Dimitrijevic", "01/23/89");
+  var playerOne = new Player(personOne, 5000, countryOne);
+  var addressOne = new Address(
+    countryOne,
+    "Belgrade",
+    11000,
+    "Kneza Milosa",
+    18
+  );
+  var bettingPlaceOne = new BettingPlace(addressOne);
+  var bettingHouseOne = new BettingHouse("Football World Cup Winner");
+
+  var playerTwo = createPlayer(
+    "Nikola",
+    "Mitic",
+    "01/05/1995",
+    "Serbia",
+    1.7,
+    CONTINENT.EUROPE,
+    6000
+  );
+
+  var bettingPlaceTwo = createBettingPlace(
+    countryOne,
+    "Cuprija",
+    35230,
+    "Orasacka",
+    14
+  );
+
+  bettingPlaceOne.addPlayers(playerOne);
+  bettingPlaceOne.addPlayers(playerTwo);
+  bettingPlaceTwo.addPlayers(playerOne);
+  bettingPlaceTwo.addPlayers(playerTwo);
+
+  bettingHouseOne.addbettingPlaces(bettingPlaceOne);
+  bettingHouseOne.addbettingPlaces(bettingPlaceTwo);
+
+  console.log(bettingHouseOne.getData());
+  console.log();
+})();
